5b8c7d6d6931e6c79fe4bbac45047fe638d1b526
MattStrong <mast4878@colorado.edu>
2019-12-10 23:17:42 -0700

    deleted notebooks

--- random_stat_generator.py
+++ random_stat_generator.py
@@ -5,6 +5,4 @@ import cherry_picker.RandomSelect as CPRandomSelect
 
 # just some fun generating some random nba player statistics.
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--legend", type=str, default='docs/legends.txt', help="path to NBA legends file")
-    CPRandomSelect.get_random_player('legend_players.csv')
+    CPRandomSelect.get_random_player(np.random.choice(['legend_players.csv', 'all_players.csv']))
